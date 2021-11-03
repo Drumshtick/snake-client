fb1be6da52f84d64d854eba89bef7419f6b84840
Drumshtick <ekodnar@outlook.com>
2021-11-03 18:38:36 +0000

    Added a key for movements as a comment, added note about having multiple event handlers and how they will be triggered in the sequence they were registered

--- client.js
+++ client.js
@@ -3,7 +3,12 @@
 // PORT: 50541
 // local: 10.0.2.15:50541
 const net = require("net");
-
+/*
+"Move: up" - move up one square (unless facing down)
+"Move: down" - move down one square (unless facing up)
+"Move: left" - move left one square (unless facing right)
+"Move: right" - move left one square (unless facing left)
+*/
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
@@ -20,9 +25,12 @@ const connect = function () {
     console.log(data);
   });
   //  Print message after a connection is established
+  // Registering multiple callbacks for the same event is totally valid.
+  // They will be triggered in the sequence that they were registered.
   conn.on("connect", () => {
     console.log("--==[Success]==-- Connected to snek server!");
     conn.write("Name: EK");
+
   });
 
   return conn;
