cd3cc56a6316df024dccc6b88950ed1f2b80db55
Drumshtick <ekodnar@outlook.com>
2021-11-03 18:28:15 +0000

    Setup handler to reciev a message from the server, send snake name to server for display

--- client.js
+++ client.js
@@ -1,18 +1,30 @@
 // establishes a connection with the game server
+//IP: 165.227.47.243
+// PORT: 50541
+// local: 10.0.2.15:50541
 const net = require("net");
 
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
     port: "50541",
+    // host: "165.227.47.243",
+  //   port: "50541",
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
+  // Receive from the server 
   conn.on("data", (data) => {
+    console.log("--==[FROM SERVER]==--");
     console.log(data);
   });
+  //  Print message after a connection is established
+  conn.on("connect", () => {
+    console.log("--==[Success]==-- Connected to snek server!");
+    conn.write("Name: EK");
+  });
+
   return conn;
 };
 
--- play.js
+++ play.js
@@ -1,7 +1,4 @@
 //main file through which you will launch the game client
-//IP: 165.227.47.243
-// PORT: 50541
-// local: 10.0.2.15:50541
 const {connect} = require('./client');
 
 
