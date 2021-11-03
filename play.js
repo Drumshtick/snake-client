c49dff15a20ef6f3c383fd93cfbe95c8c80f30fb
Drumshtick <ekodnar@outlook.com>
2021-11-03 18:49:14 +0000

    Setup a user interface function in play.js, captures all stdin and listens for Ctrl-c to exit

--- play.js
+++ play.js
@@ -1,7 +1,24 @@
 //main file through which you will launch the game client
 const {connect} = require('./client');
 
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log("--==[Ctrl-C]==-- Exiting....");
+    process.exit();
+  }
+};  
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
 
 console.log("Connecting ...");
 connect();
-
+setupInput();
