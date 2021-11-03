0abc9a9ee1519e69a37afda81644615364370b83
Drumshtick <ekodnar@outlook.com>
2021-11-03 18:13:25 +0000

    moved tcp connection code from play to clint file

--- play.js
+++ play.js
@@ -2,23 +2,8 @@
 //IP: 165.227.47.243
 // PORT: 50541
 // local: 10.0.2.15:50541
-const net = require("net");
+const {connect} = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: "50541",
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
